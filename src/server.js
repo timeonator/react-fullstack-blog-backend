d80a1fa76f0541472df89c66bde83d64d90f36d6
timeonator <timeonator@gmail.com>
2021-03-22 23:27:21 -0700

    initial checkin - not much here yet :)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser,{json} from 'body-parser';
 
 const app = express();
 
+app.use(json())
+
 app.get('/hello',(req,res) => res.send('hello'));
+app.post('/hello',(req,res) => res.send(`Hello ${req.body.name}!`));
+app.get('/hello/:name',(req,res) => res.send(`Hello ${req.params.name}!`));
 
 app.listen(8000, () => console.log("listening on port 8000:"));
