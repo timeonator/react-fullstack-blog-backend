2c3c805605fa185891daf1e2de36fdc4adbc195f
timeonator <timeonator@gmail.com>
2021-03-23 02:47:44 -0700

    added comments and upvotes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,35 @@
 import express from 'express';
 import bodyParser,{json} from 'body-parser';
-
 const app = express();
 
+const articleInfo = {
+    'learn-react': {
+        upvotes: 0,
+        comments: [],
+    },
+    'learn-node': {
+        upvotes: 0,
+        comments: [],
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+        comments: [],
+    },
+}
+
 app.use(json())
+app.post('/api/articles/:name/upvote', (req,res) => {
+    let name = req.params.name;
+    articleInfo[name].upvotes += 1;
+    res.status(200).send(`${name} now has ${articleInfo[name].upvotes} upvotes!`);
+});
+app.post('/api/articles/:name/add-comment', (req,res) =>{
+    let name = req.params.name;
+    let {username,text} = req.body
+    console.log (name, username, text);
+    articleInfo[name].comments.push({username,text});
+    res.status(200).send(articleInfo[name]);
+} )
 
 app.get('/hello',(req,res) => res.send('hello'));
 app.post('/hello',(req,res) => res.send(`Hello ${req.body.name}!`));
