5d14b15c60319462a4aeabb226cca0582dd50fc2
timeonator <timeonator@gmail.com>
2021-03-27 03:34:59 -0700

    /api/article:name working

--- src/server.js
+++ src/server.js
@@ -1,21 +1,8 @@
 import express from 'express';
 import bodyParser,{json} from 'body-parser';
-const app = express();
+import MongoClient from 'mongodb'
 
-const articleInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
-    },
-}
+const app = express();
 
 app.use(json())
 app.post('/api/articles/:name/upvote', (req,res) => {
@@ -29,7 +16,47 @@ app.post('/api/articles/:name/add-comment', (req,res) =>{
     console.log (name, username, text);
     articleInfo[name].comments.push({username,text});
     res.status(200).send(articleInfo[name]);
-} )
+})
+app.get('/api/article/all', (async (req,res) => {
+    const uri = 'mongodb://localhost:27017';
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        const db = client.db('myDB');
+        const articles = db.collection('articles');
+        const query = "{}"; const options = "{}";
+        const cursor = articles.find(query);
+        const articleInfo = (await cursor.toArray());
+        await client.close();
+        res.send(articleInfo);
+    } 
+    catch(error) {
+        res.send(500).send({message:'Something went wrong', error});      
+    }
+}));
+app.get('/api/article/:name', (async (req,res) => {
+    const uri = 'mongodb://localhost:27017';
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        const db = client.db('myDB');
+        const articles = db.collection('articles');
+        const query = {name:articleName}; const options = {};
+        const articleInfo = articles.find(query).toArray();
+        res.send(await articleInfo);   
+        await client.close();
+
+    } 
+    catch(error) {
+        res.send(500).send({message:'Something went wrong', error});      
+    }
+}));
 
 app.get('/hello',(req,res) => res.send('hello'));
 app.post('/hello',(req,res) => res.send(`Hello ${req.body.name}!`));
